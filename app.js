b18987163b7fb13d4e366b8bf90fc8cdf65db094
Asuzaka <ligvadogamer@gmail.com>
2024-07-23 10:09:16 +0500

    media added

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 let movers = document.querySelector('#movers')
 let scrollCard = document.querySelector('#scroll')
+let Overlay = document.getElementById('overlay')
+let switchBtn = document.querySelector('.hamb-btn')
+let HCloseBtn = document.querySelector('#h-close-btn')
 
+function OpenMenu() {
+    document.querySelector('.hamburger').classList.remove('d-none')
+    Overlay.classList.remove('d-none')
+}
+
+function CloseMenu() {
+    document.querySelector('.hamburger').classList.add('d-none')
+    Overlay.classList.add('d-none')
+}
+
+switchBtn.addEventListener('click', (e) => {
+    e.stopPropagation()
+    OpenMenu()
+})
+HCloseBtn.addEventListener('click', (e) => {
+    e.stopPropagation()
+    CloseMenu()
+})
+
+
+Overlay.addEventListener('mouseup', (e) => {
+    e.stopPropagation()
+    CloseMenu()
+})
+
+
+
+
+// Scroll
 movers.children[0].addEventListener('click', (e) => {
     e.stopPropagation()
     scrollCard.scrollLeft -= 1200;
